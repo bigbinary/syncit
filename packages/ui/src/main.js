da0d8fa8786d570dcccbbc2fe756700f973831b5
Mohit <mohitharshan.mec@gmail.com>
2022-11-15 13:10:32 +0530

    separated ui from replayer

--- packages/ui/src/main.js
+++ packages/ui/src/main.js
@@ -1,4 +1,4 @@
-import App from './App.svelte';
+// import App from './App.svelte';
 import Embed from './Embed.svelte';
-
-export { App, Embed };
+import Replayer from "./Replayer.svelte"
+export {  Embed, Replayer };
